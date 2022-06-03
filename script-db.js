ae38528de12eef62a2604054cfb4a8e8c4a8a5e6
Danier Fresley Vanegas Becerra <danierfresley@gmail.com>
2022-06-03 17:28:31 -0500

    Se agrega consulta
    Consulta a la base de datos Web SQL

--- script-db.js
+++ script-db.js
@@ -1,6 +1,8 @@
+var db = null;
+
 addEventListener("load", beginCreateDB);
 function beginCreateDB() {
-	var db = openDatabase('mydb', '1.0', 'My first database', 2 * 1024 * 1024);
+	db = openDatabase('mydb', '1.0', 'My first database', 2 * 1024 * 1024);
 	db.transaction(consultaDB, errorCDB,exitCDB);
 }
 
@@ -8,12 +10,18 @@ function consultaDB(tx) {
   //Crear la tabla contactos si no esta creada
   tx.executeSql('CREATE TABLE IF NOT EXISTS Contactos (id, nombre, email)');
 	//Insertar datos en la tabla
-	tx.executeSql('INSERT INTO Contactos (id, nombre, email) VALUES (1, "Jose Cordova","jc@gmail.com")');
-	tx.executeSql('INSERT INTO Contactos (id, nombre, email) VALUES (2, "Maria Alpei","ma@gmail.com")');
+	//tx.executeSql('INSERT INTO Contactos (id, nombre, email) VALUES (1, "Jose Cordova","jc@gmail.com")');
+	//tx.executeSql('INSERT INTO Contactos (id, nombre, email) VALUES (2, "Maria Alpei","ma@gmail.com")');
+	tx.executeSql('SELECT id, nombre, email FROM Contactos',[],(tx, results) => {
+		var lista = [];
+		for(var i =0; i < results.rows.length; i++){
+			lista.push('<li>' + results.rows.item(i).nombre +', '+results.rows.item(i).email  + '</li>');
+		}
+		document.getElementById('resultados').innerHTML = lista.join('');
+	});
 }
 
 // función para capturar el error en la transacción
-
 function errorCDB(error) {
 	alert("Error en la consulta SQL: "+error.code);
 }
